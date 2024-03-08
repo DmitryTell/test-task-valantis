68246fb72d756994408580b30a15acaf8496eff1
DmitryTell <dmitrij.teleganov@gmail.com>
2024-03-08 14:59:39 +0300

    Updated

--- src/helpers/get-current-date.ts
+++ src/helpers/get-current-date.ts
@@ -3,7 +3,7 @@ const addZeroBefore = (number: string) => (number.length < 2 ? `0${number}` : nu
 export const getCurrentDate = () => {
   const date = new Date();
   const year = date.getFullYear();
-  const month = date.getMonth();
+  const month = date.getMonth() + 1;
   const day = date.getDate();
 
   return `${year}${addZeroBefore(String(month))}${addZeroBefore(String(day))}`;
